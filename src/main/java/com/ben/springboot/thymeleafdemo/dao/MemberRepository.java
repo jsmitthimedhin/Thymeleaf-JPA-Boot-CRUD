10c7b115c5bdabd6ddfb4fb9bb604fb5590e1d1a
Jirayu Smitthimedhin (Ben) <65589688+jsmitthimedhin@users.noreply.github.com>
2021-07-16 12:38:12 +0700

    Add files via upload
    Added search capabilities

--- src/main/java/com/ben/springboot/thymeleafdemo/dao/MemberRepository.java
+++ src/main/java/com/ben/springboot/thymeleafdemo/dao/MemberRepository.java
@@ -3,6 +3,8 @@ package com.ben.springboot.thymeleafdemo.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.ben.springboot.thymeleafdemo.entity.Member;
 
@@ -10,4 +12,7 @@ import com.ben.springboot.thymeleafdemo.entity.Member;
 public interface MemberRepository extends JpaRepository<Member, Integer> {
 	
 	public List<Member> findAllByOrderByLastNameAsc();
+	
+	@Query(value="SELECT * FROM member e WHERE e.first_name LIKE %:keyword% OR e.last_name LIKE %:keyword% OR e.email LIKE %:keyword%", nativeQuery=true)
+	List<Member> findByKeyword(@Param("keyword") String keyword);
 }
